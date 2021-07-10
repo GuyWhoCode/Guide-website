755bdbf5b9d5eff7153865186a155b9327afb523
GuyWhoCode <thatguywhocodes@gmail.com>
2021-07-09 19:58:08 -0700

    implemented cooldown and sockets

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const server = app.listen(3000, function() {
     console.log("Your app is listening on port " + server.address().port);
     //Local Development environment: http://127.0.0.1:3000/
 });
+const socket = require('socket.io')(server)
 
 app.use(express.static(__dirname + "/website"));
 app.use(express.static(__dirname));
@@ -12,15 +13,22 @@ app.get("/", function(request, response) {
 });
 
 const MongoClient = require('mongodb').MongoClient;
-const url = process.env.uri
+const url = "mongodb+srv://" + process.env.dbUSER + ":" + process.env.password + "@guide-info.e5dr4.mongodb.net/skyblockGuide?retryWrites=true&w=majority"
 const dbClient = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true, poolSize: 10});
-const socket = require('socket.io')(server)
+dbClient.connect((err) => {
+    console.log("Connected to database!")
+})
 
-// dbClient.connect(async (err) => {
-//     console.log("Connected to database!")
-// })
-
-module.exports = {
-    dbClient: dbClient,
-    socket: socket
-}
+const search = require("./search.js")
+socket.on('connection', io => {
+    console.log("I have a socket connection!")
+    io.on('search', async (query) => {
+            let guidesDB = dbClient.db("skyblockGuide").collection("Guides")
+            let guide = await guidesDB.find({}).toArray()
+            let searchQuery = query.trim()
+            let guideMessage = search.parseQuery(searchQuery, guide)
+            guideMessage.timestamp = new Date()
+            io.emit("searchResult", guideMessage)
+      
+    })
+})
--- website/script.js
+++ website/script.js
@@ -1,6 +1,5 @@
 let messageArea = document.getElementById('messageArea')
-let sampleGuide = {"_id":{"$oid":"5fde248f8cedc30004ac9600"},"embedMessage":{"color":{"$numberInt":"8902906"},"title":"In-depth Enchanting","fields":[{"name":"_ _","value":"_ _"},{"name":"Introduction","value":"Enchanting is the most simple skill to grind that there is currently (other than Taming). To start the grind, you first have to get Enchanting 10. You will most likely just get it automatically by enchanting stuff early on with the enchantment table. Once you reach Enchanting 10, you can create the Experimentation Table. It is crafted with 8 Oak Logs and 1 Experience Bottle (you can buy it from the Librarian in the Hub).\n​\n​To get Enchanting XP, you need to do Experimentations. Every day, you can do 1 experiment, however you can reset the countdown up to 3 times per day at the cost of Minecraft LVLs and Bits. To do the Main Experiment, you need Minecraft Levels as well as completing all the unlocked Add-Ons first. These Add-Ons will give you additional clicks in the Main Experiment.\n​"},{"name":"Experiments","value":"**Chronomatron** (Simon Says) - unlocks at Enchanting 20. \n**Maximum Amount of Clicks Needed:** Series of 12. \n**Maximum Amount of Enchanting XP: ** Series of 15.  \n\n**Ultrasequencer** - unlocks at Enchanting 25. It’s a visual memory game in which the player must memorize numeric sequences which appear for a brief amount of time and then recite the sequences by clicking on the squares where they were. \n**Maximum Amount of Clicks Needed:** Series of 9. \n**Maximum Amount of Enchanting XP: ** Series of 20.  ​\n\nSuperpairs is the Main Experiment. It’s a memory matching game, where the player reveals two pieces of the board at a time and the goal is to match a pair. For the maximum amount of XP, it is recommended to only go for the XP pairs and ignore the enchantments pairs (you can get Lvl 6 and 7 enchantments if you want). If you plan on doing the resets, it is recommended to get the Titanic Exp Bottles as it will save you a lot of money.\n​"},{"name":"Notes","value":"-Always do the highest tier Add-Ons/Experiment you can with your Enchanting LVL.\n-It is **HIGHLY** recommended to use a mod that highlights the notes/squares to pick in the Add-Ons games to make your life much easier (SBE, Danker’s Mod and Dungeon utils).\n**THESE MODS ARE CONFIRMED TO BE ALLOWED BY HYPIXEL STAFF, AS LONG AS IT DOESN’T DO THE CLICKS FOR YOU!!**\n-You can also get up to 500k Enchanting XP per day with the Enchantment Table and the Anvil!\n​"}],"footer":{"text":"SkyBlock Guides","icon_url":"https://i.imgur.com/184jyne.png"},"timestamp":{"$date":{"$numberLong":"1609046547654"}}},"categoryTitle":"In-depth Enchanting","messageID":{"587765474297905158":"844769945158287361","807319824752443472":"848316837406179339"},"category":"Skyblock"}
-// let socket = io("/");
+let socket = io("/");
 let timer = 0;
 let helpGuide = {
     "title": "Bot Commands",
@@ -54,8 +53,7 @@ const parseCommand = text => {
 
     if (commandWord === "help") return createMessage(helpGuide, "Bot", "Bot Response")
     else if (commandWord === "search") {
-        // socket.emit("search", arguments.slice(1, arguments.length).join(" "))
-        console.log("I am searching for: " +  arguments.slice(1, arguments.length).join(" "))
+        socket.emit("search", arguments.slice(1, arguments.length).join(" "))
     }
 }
 
@@ -68,7 +66,6 @@ const createEmbedMessage = (info, messageDiv) => {
     embedTitle.className = "embedTitle"
     embedTitle.innerHTML = info.title
 
-    
     info.fields.map(val => {
         if (val.name == "_ _") return;
         let embedSubtitle = document.createElement("p")
@@ -87,14 +84,13 @@ const createEmbedMessage = (info, messageDiv) => {
 }
 
 const createMessage = (message, user, type) => {
-    let msg;
     let messageDiv = document.createElement("div")
     messageDiv.className = 'message'
     
     let getTime = new Date()
     let time = document.createElement("p")
     time.className = 'date'
-    time.innerHTML = "Today at " + getTime.toLocaleTimeString().slice(0,4) + " " + getTime.toLocaleTimeString().slice(8)
+    time.innerHTML = "Today at " + getTime.toLocaleTimeString().slice(0,5) + " " + getTime.toLocaleTimeString().slice(8)
 
     let iconPic = document.createElement("img")
     iconPic.className = 'icon'
@@ -110,23 +106,29 @@ const createMessage = (message, user, type) => {
         iconPic.src = "pictures/bot.png"
         author.innerHTML = "SkyBlock Guides"
     }
-
+ 
     let nameTime = document.createElement('div')
     nameTime.className = 'nameTime'
     nameTime.appendChild(author)
     nameTime.appendChild(time)
 
+
+    let msg = document.createElement("p")
+    msg.className = 'messageBody'
+    
+    messageDiv.appendChild(msg)
     messageDiv.appendChild(iconPic)
     messageDiv.appendChild(nameTime)
     messageArea.appendChild(messageDiv)
 
     if (type === "Command" && ((Date.now() - timer)/1000) >= 10) {
-        msg = document.createElement("p")
-        msg.className = 'messageBody'
+        timer = Date.now()
         msg.innerHTML = message
-        messageDiv.appendChild(msg)
         msg.scrollIntoView()
         parseCommand(message)
+    } else if (((Date.now() - timer)/1000) < 10 && type === "Command") {
+        msg.innerHTML = message + "<br>You are on cooldown for " + (10-((Date.now()-timer)/1000)).toFixed(1) + " seconds."
+        msg.scrollIntoView()
     } else {
         createEmbedMessage(message, messageDiv)
     }
@@ -150,6 +152,6 @@ document.getElementById('userInput').addEventListener('submit', function(event) 
 
 
 createMessage(helpGuide, "Bot", "Bot Response")
-// socket.on("searchResult", result => {
-//     console.log(result)
-// })
+socket.on("searchResult", result => {
+    createMessage(result, "Bot", "Bot Response")
+})
